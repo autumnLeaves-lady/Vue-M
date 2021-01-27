71351757a108da6d815653468da4557533589cc1
menglijiao <menglijiao@southzn.com>
2021-01-27 14:01:29 +0800

    2021-01-27

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,51 @@
 <template>
-  <div>首页</div>
+  <div
+    class="shouye"
+    @mousemove="moveLayer"
+    @mouseenter="addActive"
+    @mouseleave="removeActive"
+  >
+    <div
+      v-show="visible"
+      style="position: fixed;z-index: 1"
+      ref="Layer"
+    >
+      <div style="width: 20px;height: 20px;border:1px solid #000;"></div>
+    </div>
+  </div>
+
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    addActive() {
+      this.visible = true;
+      console.log('this.visible++++++: ', this.visible);
+    },
+    removeActive() {
+      this.visible = false;
+      console.log('this.visible------: ', this.visible);
+    },
+    moveLayer(event) {
+      this.$refs.Layer.style.left = `${event.clientX - 10}px`;
+      this.$refs.Layer.style.top = `${event.clientY - 10}px`;
+    },
+  },
 
 };
 </script>
 
-<style>
-
+<style lang="scss">
+.shouye {
+  // cursor: url("../assets/normal.ico"), pointer;
+  height: 400px;
+  background-color: rgb(191, 236, 236);
+  // position: fixed;
+}
 </style>
